424a948ebdbbcfaca92333ac2c84f45f9441d2e3
hx-w <herixth@163.com>
2021-09-27 16:53:48 +0800

    -on_webconnect

--- nonebot/plugins/scubot/__init__.py
+++ nonebot/plugins/scubot/__init__.py
@@ -7,13 +7,8 @@ import redis
 from . import register
 
 bot: nonebot.NoneBot = nonebot.get_bot()
-redis_inst: redis.Redis = None
-
-@nonebot.on_websocket_connect
-async def on_connect(event: aiocqhttp.Event):
-    global redis_inst
-    redis_inst = redis.Redis(host='redis', port=6379, decode_responses=True)
-    print(f'[on_websocket_connect] {redis_inst}')
+redis_inst: redis.Redis = redis.Redis(
+    host='redis', port=6379, decode_responses=True)
 
 
 @bot.on_message('private')
